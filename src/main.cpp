02994a5b3d30313e5795ec2be1ed1fc31e6f2280
Travis McCawley <targetdog88@gmail.com>
2019-03-29 22:19:04 -0700

    Updated .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "matplotlibcpp.h"
 
+namespace plt = matplotlibcpp;
 int main() {
-    int a = 1;
-    printf("HELLO WORLD!");
-    return 0;
+    plt::plot({1,3,2,4});
+    plt::show();
 }
