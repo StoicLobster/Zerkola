60a1e4d90f57414fe79a6f69510f1b921936a4fe
Travis <trmccawley@gmail.com>
2019-04-06 13:25:48 -0700

    Successful build

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "matplotlibcpp.h"
+#include <iostream>
 
 namespace plt = matplotlibcpp;
+plt::ion();
 int main() {
-    plt::plot({1,3,2,4});
-    plt::show();
+	double dt = 1;
+	double t = 0;
+	while (t < 1000) {
+		plt::plot({5,3,2,4,t});
+		//plt::draw();
+		plt::show();
+		std::cout << t << std::endl;
+		t += dt;
+	}
 }
