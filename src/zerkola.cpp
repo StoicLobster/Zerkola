90400fbab318060f983d56639c5b050bedeeeaba
Travis <trmccawley@gmail.com>
2019-04-14 10:06:30 -0700

    Added space bar command

--- src/zerkola.cpp
+++ src/zerkola.cpp
@@ -2,7 +2,7 @@
 
 namespace zerkola {
 
-	const double kDRAW_RATE = 0.001; //draw rate [s]
+	const double kDRAW_FREQ = 60; //draw frequency [Hz]
 
 	Zerkola::Zerkola(): kNorthLimit_(100.0), kEastLimit_(100.0), kSouthLimit_(0.0), kWestLimit_(0.0),
 			kGameBoardBoundaryX_{kNorthLimit_,kNorthLimit_,kSouthLimit_,kSouthLimit_,kNorthLimit_},
@@ -29,37 +29,44 @@ namespace zerkola {
 		//Instantiate game pieces
 		geometry::PlotObj tank_player(50,50,geometry::BoundaryPoly::kTank,1.0,0.1);
 		while (true) {
-			//Read input			
-			if (getch() == '\033') { // check for esc char
-				getch(); // skip [
-				switch (getch()) {
-					case 'A':
-						printw("You pressed Up!\n");
-						tank_player.Move(true);
-						break;
-					case 'B':
-						printw("You pressed Down!\n");
-						tank_player.Move(false);
-						break;
-					case 'C':
-						printw("You pressed Right!\n");
-						tank_player.Rot2D(false);
-						break;
-					case 'D':
-						printw("You pressed Left!\n");
-						tank_player.Rot2D(true);
-						break;
-				}
-			}
-			flushinp(); //Flush input buffer
 			//Clear plot
 			plt::cla();
 			plt::axis("off");
 			plt::plot(kGameBoardBoundaryX_,kGameBoardBoundaryY_);
 			plt::tight_layout();
+			//Read player input	
+			switch (getch()) {
+				case '\033': 
+					//Escape char used for arrow keys
+					getch(); // skip [
+					switch (getch()) {
+						case 'A':
+							printw("You pressed Up!\n");
+							tank_player.Move(true);
+							break;
+						case 'B':
+							printw("You pressed Down!\n");
+							tank_player.Move(false);
+							break;
+						case 'C':
+							printw("You pressed Right!\n");
+							tank_player.Rot2D(false);
+							break;
+						case 'D':
+							printw("You pressed Left!\n");
+							tank_player.Rot2D(true);
+							break;
+					}
+					break;
+				case ' ':
+					//Space bar
+					printw("You pressed Space!\n");
+					break;
+			}
+			flushinp(); //Flush input buffer
 			//Update plot
 			tank_player.Plot();
-			plt::pause(kDRAW_RATE);
+			plt::pause(1/kDRAW_FREQ);
 		}
 
 		return;
