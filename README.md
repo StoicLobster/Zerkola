6f7d0f361df5340d40a21546067f7dab3fd8b8c3
Travis McCawley <trmccawley@gmail.com>
2019-04-20 20:06:02 -0700

    Switch to new dev system, added LICENSE, and put matplotlib-cpp in repo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Zerkola
 A space tanks battle game!
+
+Dependencies:
+- Eigen 3.3.7
+- ncurses
+- python2.7
+  - matplotlib
+  - numpy
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+
+// Copyright (c) 2019 Travis McCawley. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+// Credits:
+// matplotlib-cpp by Benno Evers: https://github.com/lava/matplotlib-cpp
 #include "zerkola.h"
 #include <iostream>
 
